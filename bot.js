a8354674f2eadfc702c153b3006d8f25c97dbc6a
britnesy <68761235+britnesy@users.noreply.github.com>
2020-07-26 16:28:26 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 
-const client = new Discord.Client(732749137975705680);
+const client = new Discord.Client();
 
  
 
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.NzMyNzQ5MTM3OTc1NzA1Njgw.Xw5H0A.y0vHGGkEnz_qptBY3aWjDY6RWy0);//where BOT_TOKEN is the token of our bot
+client.login(process.env.token);
